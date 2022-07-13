1973ebc1ca32b3d63915f0a504aacfccc1eedccc
ddefo25 <ispumamm@gmail.com>
2022-07-13 21:40:18 +0200

    remarks fix

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,17 +1,13 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  if (arr1.length !== arr2.length) {
+    return false
+  };
 
-  result = arr1.every((element, index) => {
+  return arr1.every((element, index) => {
     return arr1.length === arr2.length && element === arr2[index];
   });
-
-  return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  resultArr = arr.filter(element => element > 0 && element % 3 === 0).map(element => element * 10)
-
-  return resultArr; // array
+  return resultArr = arr.filter(element => element > 0 && element % 3 === 0).map(element => element * 10)
 }
