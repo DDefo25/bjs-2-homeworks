7d9f81eb3c6614ee1f9ce630437e6d16a47f4402
ddefo25 <ispumamm@gmail.com>
2022-07-17 11:50:43 +0200

    task 4 is completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
+};
+
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+  };
 }
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function (...marks) {
+  if(this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(...marks);
+  } else {
+    this.marks.push(...marks);
+  };
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((sum, current) => { return sum + current }, 0) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
