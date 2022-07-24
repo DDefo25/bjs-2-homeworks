d6a92a661f0da104d7af70578e6c14529c157b3e
ddefo25 <ispumamm@gmail.com>
2022-07-24 15:18:19 +0200

    task 6.2 is completed

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -11,3 +11,34 @@ function validateCount(value) {
         return error;
     }
 }
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b < c || b + c < a || c + a < b) throw new Error('Треугольник с такими сторонами не существует');
+        this.sides = [a, b, c];
+    }
+
+    getPerimeter() {
+        return this.sides.reduce((sum, value) => sum + value);
+    }
+
+    getArea() {
+        let halfPerimeter = this.getPerimeter() / 2;
+        return Number(Math.sqrt(halfPerimeter * this.sides.reduce((sum, value) => sum * (halfPerimeter - value), 1)).toFixed(3))
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch(error) {
+        return {
+            getPerimeter() {
+                return 'Ошибка! Треугольник не существует'
+            },
+            getArea() {
+                return 'Ошибка! Треугольник не существует'
+            }
+        }
+    }
+}
