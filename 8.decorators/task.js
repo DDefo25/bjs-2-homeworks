36506286a0806ec2de5455134c331121c9b59370
ddefo25 <ispumamm@gmail.com>
2022-07-26 12:29:02 +0200

    tasks 8.1 & 8.2 are completed

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,10 +1,45 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  return function(...args) {
+    const hash = args.join(',');
+    const findHash = cache.find(item => item.hash === hash);
+    if (findHash) {
+      return `Из кэша: ${findHash.result}`;
+    }
+
+    const result = func(...args);
+
+    if (cache.length >= 5) {
+      cache.splice(0, 1);
+    }
+
+    cache.push({
+      hash,
+      result,
+    });
+
+    return `Вычисляем: ${result}`;
+  }
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+  let isFirst = false;
+  return function(...args) {
+    setTimeout(() => {
+      isFirst = false;
+    }, delay)
+
+    if (isFirst) {
+      setTimeout(() => {
+        isFirst = false;
+      }, delay)
+      return;
+    }
+
+    func(...args);
+    isFirst = true;
+  }
 }
 
 function debounceDecorator2(func) {
